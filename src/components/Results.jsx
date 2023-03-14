dae3e566e9c01ee2ec5caaecacf1e93befbfb086
Luca <lucabernacchia98@gmail.com>
2023-03-14 13:45:09 -0300

    change-message

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -5,7 +5,7 @@ export const Results = ({ result, options, userChoice, computerChoice, reset }) 
   return (
     result !== null && (
       <div>
-        {result === 0 && <p className='message'>🤷🏽‍♀️ It's a tie</p>}
+        {result === 0 && <p className='message'>🤷🏽‍♀️ It's a draw</p>}
         {result === 1 && (
           <p className='message'>
             ✅ You won with {options[userChoice]?.name} against {" "}
